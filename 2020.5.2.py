31e3293699c53c61cf2c1b5f30d1eb85cd148738
Gregory <gregory.buser@gmail.com>
2022-07-04 11:33:20 -0500

    day 5 part 2

---
+++ 2020.5.2.py
@@ -0,0 +1,14 @@
+data = [x.rstrip() for x in open('data.txt', 'r')]
+keys = (('F', '0'), ('B', '1'), ('L', '0'), ('R', '1'))
+
+seats = []
+for seat in data:
+    for key, value in keys:
+        seat = seat.replace(key, value)
+    seats.append(int(seat, 2))
+    
+        
+seats.sort()
+missing_seats = [x for x in range(len(seats)) if x not in seats]
+
+print([x for x in missing_seats if (x-1)not in missing_seats and (x+1) not in missing_seats])
