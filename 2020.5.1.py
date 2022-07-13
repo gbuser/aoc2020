df6265ce6200ac6ba21ed143b749521094e47784
Gregory <gregory.buser@gmail.com>
2022-07-13 12:22:23 -0500

    added comments to day 5 part 1

--- 2020.5.1.py
+++ 2020.5.1.py
@@ -1,10 +1,12 @@
 data = [x.rstrip() for x in open('data.txt', 'r')]
+
+#note that each 10 digit string is just encoded 10 bit binary
 keys = (('F', '0'), ('B', '1'), ('L', '0'), ('R', '1'))
 
 max_seat_ID = 0
 for seat in data:
     for key, value in keys:
-        seat = seat.replace(key, value)
+        seat = seat.replace(key, value) #replace F,B,L and R with 0 or 1
     seat_ID = int(seat, 2)
     if seat_ID > max_seat_ID:
         max_seat_ID = seat_ID
